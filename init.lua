3cbce1d4ea071efdea55d3ab7396e7520533d989
Vanand Gasparyan <vgasparyan1995@gmail.com>
2025-04-21 14:02:48 +0200

    Configure nvim-tree to show gitingnored files by default.

--- init.lua
+++ init.lua
@@ -93,7 +93,11 @@ require("lazy").setup({
         "nvim-tree/nvim-web-devicons",
       },
       config = function()
-        require("nvim-tree").setup {}
+        require("nvim-tree").setup {
+          git = {
+            ignore = false,
+          }
+        }
       end,
     },
 
