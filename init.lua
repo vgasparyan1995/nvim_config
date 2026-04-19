51f3c462aea43552033823567481b89ce3934c2c
Vanand Gasparyan <vgasparyan1995@gmail.com>
2026-04-19 17:18:47 +0200

    Improves markview.

--- init.lua
+++ init.lua
@@ -192,6 +192,7 @@ vim.api.nvim_set_keymap('n', '<leader>jf', '<cmd>%!jq .<cr>', { noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>mm', '<cmd>Markview<cr>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>ms', '<cmd>Markview splitToggle<cr>', { noremap = true})
 vim.keymap.set('n', '<leader>gf', find_file_under_cursor, { noremap = true, silent = true })
 
 vim.cmd[[colorscheme tokyonight-night]]
